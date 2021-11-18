07de22659d0e0d4b5de6ced67341e95f73cc8857
adilsonfuta <adilson.futa@jupiter.co.ao>
2021-11-18 11:51:47 +0000

    novo controller

--- src/bonus/bonus.controller.ts
+++ src/bonus/bonus.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
-
+import {
+ Controller,
+ Get,
+ Post,
+ Put,
+ Delete,
+ Body,
+ Param,
+ Header
+} from '@nestjs/common';
+import { get } from 'http';
 @Controller('bonus')
-export class BonusController {}
+export class BonusController {
+  @Get()
+  @Header('Cache-Control','none')
+  listAll(): string {
+    return 'retornando os valores';
+}
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This actions return ${id} value`;
+}
+
+  @Post()
+  create(): string {
+    return 'create new bonus';
+}
+
+
+}
